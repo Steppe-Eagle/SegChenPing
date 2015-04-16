cf529b04df9e814c0ffe43b3f0cec323d4796da2
lmhieu612 <lmhieu612@gmail.com>
2015-04-15 23:02:12 -0400

    revise for cluster

---
+++ libsvx.v3.0/swa/jobswa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+/nfs/bigeye/hieule/swa/swa /nfs/bigeye/hieule/libsvx.v3.0/swa/configfile/bird_of_paradise.txt
+/nfs/bigeye/hieule/swa/swa /nfs/bigeye/hieule/libsvx.v3.0/swa/configfile/drift.txt
+/nfs/bigeye/hieule/swa/swa /nfs/bigeye/hieule/libsvx.v3.0/swa/configfile/frog.txt
+/nfs/bigeye/hieule/swa/swa /nfs/bigeye/hieule/libsvx.v3.0/swa/configfile/worm.txt
+/nfs/bigeye/hieule/swa/swa /nfs/bigeye/hieule/libsvx.v3.0/swa/configfile/parachute.txt
