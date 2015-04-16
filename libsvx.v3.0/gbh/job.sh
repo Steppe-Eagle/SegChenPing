87576ec1c4fad118e0ae1a6a6cf0875fc607ab37
lmhieu612 <lmhieu612@gmail.com>
2015-04-15 23:24:26 -0400

    fix shell file

---
+++ libsvx.v3.0/gbh/job.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/nfs/bigeye/hieule/libsvx.v3.0/gbh/gbh 5 500 200 0.5 20 /nfs/bigeye/hieule/dataset/SegTrackv2/PPMImages/frog /nfs/bigeye/hieule/output/gbh/frog
+/nfs/bigeye/hieule/libsvx.v3.0/gbh/gbh 5 500 200 0.5 20 /nfs/bigeye/hieule/dataset/SegTrackv2/PPMImages/worm /nfs/bigeye/hieule/output/gbh/worm
+
+
